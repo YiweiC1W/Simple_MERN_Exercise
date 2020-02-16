d37d7e82ceef7d4f3303243b0c563c7e717370f3
Yiwei Chen <kanyeishere@gmail.com>
2020-02-16 22:40:52 +1100

    frontend route and face detect function

---
+++ src/App.js
@@ -0,0 +1,200 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkFrom from './components/ImageLinkFrom/ImageLinkFrom';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import './App.css';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+	apiKey: '821599bfad5144b79f163035172d7539'
+});
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			box: {},
+			route: 'signin',
+			isSignedIn: false
+		};
+	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height
+		};
+	};
+
+	displayFaceBox = (box) => {
+		this.setState({ box: box });
+	};
+
+	onInputChange = (event) => {
+		this.setState({ input: event.target.value });
+	};
+
+	onButtonSubmit = () => {
+		this.setState({ imageUrl: this.state.input });
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			.then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.catch((err) => console.log(err));
+	};
+
+	onRouteChange = (route) => {
+		if (route === 'signin') {
+			this.setState({ isSignedIn: false });
+		} else if (route === 'home') {
+			this.setState({ isSignedIn: true });
+		}
+		this.setState({ route: route });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles" params={particleOptions} />
+				<Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn} />
+				{this.state.route === 'home' ? (
+					<div>
+						<Logo />
+						<Rank />
+						<ImageLinkFrom onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+						<FaceRecognition box={this.state.box} imgUrl={this.state.imageUrl} />
+					</div>
+				) : this.state.route === 'signin' ? (
+					<Signin onRouteChange={this.onRouteChange} />
+				) : (
+					<Register onRouteChange={this.onRouteChange} />
+				)}
+			</div>
+		);
+	}
+}
+
+const particleOptions = {
+	particles: {
+		number: {
+			value: 80,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		},
+		color: {
+			value: '#ffffff'
+		},
+		shape: {
+			type: 'circle',
+			stroke: {
+				width: 0,
+				color: '#000000'
+			},
+			polygon: {
+				nb_sides: 5
+			},
+			image: {
+				src: 'img/github.svg',
+				width: 100,
+				height: 100
+			}
+		},
+		opacity: {
+			value: 0.5,
+			random: false,
+			anim: {
+				enable: false,
+				speed: 1,
+				opacity_min: 0.1,
+				sync: false
+			}
+		},
+		size: {
+			value: 3,
+			random: true,
+			anim: {
+				enable: false,
+				speed: 40,
+				size_min: 0.1,
+				sync: false
+			}
+		},
+		line_linked: {
+			enable: true,
+			distance: 150,
+			color: '#ffffff',
+			opacity: 0.4,
+			width: 1
+		},
+		move: {
+			enable: true,
+			speed: 6,
+			direction: 'none',
+			random: false,
+			straight: false,
+			out_mode: 'out',
+			bounce: false,
+			attract: {
+				enable: false,
+				rotateX: 600,
+				rotateY: 1200
+			}
+		}
+	},
+	interactivity: {
+		detect_on: 'canvas',
+		events: {
+			onhover: {
+				enable: true,
+				mode: 'repulse'
+			},
+			onclick: {
+				enable: true,
+				mode: 'push'
+			},
+			resize: true
+		},
+		modes: {
+			grab: {
+				distance: 400,
+				line_linked: {
+					opacity: 1
+				}
+			},
+			bubble: {
+				distance: 400,
+				size: 40,
+				duration: 2,
+				opacity: 8,
+				speed: 3
+			},
+			repulse: {
+				distance: 200,
+				duration: 0.4
+			},
+			push: {
+				particles_nb: 4
+			},
+			remove: {
+				particles_nb: 2
+			}
+		}
+	},
+	retina_detect: true
+};
+
+export default App;
